4da1a8807b85b448a0be6513b91e4bab6f1c1d05
NurYilmaz <nur-yilmaz1996@hotmail.com>
2019-09-29 22:47:17 +0300

    second commit

---
+++ BricksBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BricksBreaker.Data;
+
+public class Brick : MonoBehaviour
+{
+    [SerializeField]
+    private Text BrickText;
+    public BrickData BrickData { get; set; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ BricksBreaker/Assets/Scripts/Data/BrickData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BricksBreaker.Data
+{
+    public class BrickData
+    {
+        public int Value { get; set; }
+        public int Order { get; set; }
+
+        public BrickData(int value, int order)
+        {
+            Value = value;
+            Order = order;
+        }
+    }
+}
+
